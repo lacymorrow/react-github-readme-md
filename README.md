c9999672aa504bf112f51a5bd7ecf5b4c8f3d603
Lacy Morrow <me@lacymorrow.com>
2023-09-24 23:15:25 -0400

    add storybook

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-github-readme-md
-Render the README.md file of a GitHub Repository in React
+> Render the README.md file of a GitHub Repository in React
+
+Try switching your system between light and dark mode or add a `.dark` class to the `body` element to see the dark theme.
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 
 import packageJson from "./package.json" assert { type: "json" };
@@ -23,10 +25,12 @@ export default [
       },
     ],
     plugins: [
+			peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
 			postcss(),
+			terser(),
     ],
   },
   {
