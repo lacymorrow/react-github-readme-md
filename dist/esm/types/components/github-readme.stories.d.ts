90025b42d7aff8cb60105cc8d035267f36f5f054
Lacy Morrow <me@lacymorrow.com>
2024-09-08 18:22:50 -0400

    update-deps

--- dist/esm/types/components/github-readme.stories.d.ts
+++ dist/esm/types/components/github-readme.stories.d.ts
@@ -1,20 +1,7 @@
-import { type StoryFn } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import GitHubReadme from "./github-readme";
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0a347bb9").R, {
-    username?: string | undefined;
-    repo?: string | undefined;
-    src?: string | undefined;
-    className?: string | undefined;
-    addHeadingIds?: boolean | undefined;
-    linkify?: boolean | undefined;
-}>;
-export default _default;
-export declare const LightMode: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, {
-    username?: string | undefined;
-    repo?: string | undefined;
-    src?: string | undefined;
-    className?: string | undefined;
-    addHeadingIds?: boolean | undefined;
-    linkify?: boolean | undefined;
-}>;
-export declare const DarkMode: StoryFn<typeof GitHubReadme>;
+declare const meta: Meta<typeof GitHubReadme>;
+export default meta;
+type Story = StoryObj<typeof GitHubReadme>;
+export declare const LightMode: Story;
+export declare const DarkMode: Story;
